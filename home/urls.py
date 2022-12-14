2b0a4678111916f0a370194b81e58fa3564721cb
Manoj Khanal <asmankhanal0@gmail.com>
2022-12-14 17:24:43 +0545

    first commit

---
+++ home/urls.py
@@ -0,0 +1,11 @@
+from .views import *
+from django.urls import path
+
+urlpatterns = [
+    path('', home,name="home"),
+    path('about', about, name="about"),
+    path('contact', contact, name="contact"),
+    path('price', price, name="price"),
+    path('portfolio', portfolio, name="portfolio"),
+    path('service', service, name="service"),
+]
